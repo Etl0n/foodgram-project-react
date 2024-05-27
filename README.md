038b5329d89eb4cbd2eacb637e5e8e8290741fd4
etl0n <andreynokia04@gamil.com>
2024-05-27 21:04:34 +1000

    Issued README.md

--- README.md
+++ README.md
@@ -1,49 +1,47 @@
 # Продуктовый помощник
-### Технологии:
+
+## Технологии:
 [![Python](https://img.shields.io/badge/-Python-464646?style=flat-square&logo=Python)](https://www.python.org/) [![Django](https://img.shields.io/badge/-Django-464646?style=flat-square&logo=Django)](https://www.djangoproject.com/) [![Django REST Framework](https://img.shields.io/badge/-Django%20REST%20Framework-464646?style=flat-square&logo=Django%20REST%20Framework)](https://www.django-rest-framework.org/) [![PostgreSQL](https://img.shields.io/badge/-PostgreSQL-464646?style=flat-square&logo=PostgreSQL)](https://www.postgresql.org/) [![Nginx](https://img.shields.io/badge/-NGINX-464646?style=flat-square&logo=NGINX)](https://nginx.org/ru/) [![gunicorn](https://img.shields.io/badge/-gunicorn-464646?style=flat-square&logo=gunicorn)](https://gunicorn.org/) [![docker](https://img.shields.io/badge/-Docker-464646?style=flat-square&logo=docker)](https://www.docker.com/)
+
 ## Описание проекта:
-
 #### Foodgram - это онлайн-сервис, который представляет собой «Продуктовый помощник». На этом сервисе пользователи могут делиться своими рецептами, подписываться на публикации других пользователей, добавлять понравившиеся рецепты в список «Избранное», подписываться на понравившихся им пользователей. Кроме того, они могут скачивать сводный список продуктов, необходимых для приготовления выбранных блюд перед походом в магазин.
-## Запуск проекта на сервере:
 
-#### Склонировать репозиторий
+## Как запустить приложение:
+### 1. Запуск приложения локально(лучше пользоваться этим вариантом так как он более простой):
+#### Склонировать репозиторий на свой компьютер
 > git@github.com:Etl0n/foodgram-project-react.git
-
-## Подготовка сервера:
-
-#### Обновить индекс пакетов APT
->sudo apt update 
-
-#### Обновите установленные в системе пакеты и установите обновления безопасности
->sudo apt upgrade -y
-
-#### Установить менеджер пакетов pip, утилиту для создания виртуального окружения venv, систему контроля версий git, чтобы клонировать ваш проект.
->sudo apt install python3-pip python3-venv git -y
-
-#### Установите на свой сервер Docker
->sudo apt install docker.io
-
-#### Установите docker-compose
->sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
->sudo chmod +x /usr/local/bin/docker-compose
-
+#### Создать файл .env с данными для подключения к БД
+* POSTGRES_DB = 
+* POSTGRES_USER =
+* POSTGRES_PASSWORD =
+* DB_NAME = 
+* DB_HOST = db
+* DB_PORT = 5432
+#### Собрать докер-контейнеры в единую сеть для этого выполните эту команду из директории где лежит файл docker-compose.production.yml
+> docker compose -f docker-compose.production.yml up -d
+#### По очередно выполните эти команды в этой же директории
+* >docker compose -f docker-compose.production.yml exec backend python manage.py migrate
+* >docker compose -f docker-compose.production.yml exec backend python manage.py import_csv ./data/ingredients.csv
+* >docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic
+* >docker compose -f docker-compose.production.yml exec backend cp -r /app/collected_static/. /backend_static/static/
+#### После проделанной работы приложение будет доступно по ссылке (https://localhost:7000)
+### 2. Запуск прилоения на сервере(для этого на сервере уже должен быть установлен docker и nginx, прописана конфигурация файла nginx чтобы он перенаправлял все запросы приходящие на сервер(proxy_pass http://127.0.0.1:7000)):
+#### Форкнуть репозиторий себе в GitHub:
+> Для этого найдите кнопку Fork немного выше и правее центра экрана
+#### Склонировать форкнутый репозиторий себе на компьютер:
+> git clone (SSH ссылка на форкнутый репозиторий)
 #### Добавьте в Secrets GitHub переменные окружения:
-
-* DOCKER_USERNAME (имя на DockerHub)
-* DOCKER_PASSWORD (пароль на DockerHub)
+>Для начала перейдите в Settings репозитория, затем Secrets and variables, добавте эти значения
+* DOCKER_USERNAME (имя на свой DockerHub)
+* DOCKER_PASSWORD (пароль на свой DockerHub)
 * HOST (ip адрес вашего сервера)
 * USER (имя пользователя на сервере)
 * SSH_KEY (SSH ключ от сервера)
 * TELEGRAM_TO (ваш id в телеграме, чтобы отправить уведомления о успешном деплое проекта на сервер)
 * TELEGRAM_TOKEN (TOKEN любого вашего бота в Telegram)
 
-#### Со своего компьютера сделать коммит, чтобы деплой на сервер произошел автоматически
-
-#### Собрать контейнеры на удалённом сервере
->sudo docker compose -f docker-compose.production.yml up --build -d
-
-## Проект доступен по [адресу](https://foodgramworld.ddns.net/)
+#### Со своего компьютера сделать push из репозитория, чтобы деплой на сервер произошел автоматически
+> git push
 
 ## Автор
 https://github.com/Etl0n (Ученик Яндекс Практикум)
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,109 +9,107 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = (
-    'django-insecure-ky&&2r9-k6&w^(jwu^4dwk!n_tm8-r!656m9dqf9j6@(uzr*9&'
-)
+SECRET_KEY = "django-insecure-ky&&2r9-k6&w^(jwu^4dwk!n_tm8-r!656m9dqf9j6@(uzr*9&"
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'foodgramworld.ddns.net']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "foodgramworld.ddns.net"]
 
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'food_api.apps.FoodApiConfig',
-    'recipe.apps.RecipeConfig',
-    'core.apps.CoreConfig',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'django_filters',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "food_api.apps.FoodApiConfig",
+    "recipe.apps.RecipeConfig",
+    "core.apps.CoreConfig",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'foodgram.urls'
+ROOT_URLCONF = "foodgram.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'foodgram.wsgi.application'
+WSGI_APPLICATION = "foodgram.wsgi.application"
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "django"),
+        "USER": os.getenv("POSTGRES_USER", "django"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", 5432),
     }
 }
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 6,
 }
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -120,13 +118,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
 
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/media/"
 
-handler404 = 'core.views.page_not_found'
+handler404 = "core.views.page_not_found"
